6bbe05c47f1b69a870bc0dc65432c7f62460ce1e
LoKe112 <ivan1200100@mail.ru>
2025-12-23 20:50:08 +0400

    fix connection

--- Hospital/src/Hospital.RabbitMqProducer/Program.cs
+++ Hospital/src/Hospital.RabbitMqProducer/Program.cs
@@ -8,18 +8,28 @@ builder.Services.AddLogging();
 
 builder.AddRabbitMQClient("RabbitMQ");
 
-builder.Services.AddServiceDiscovery();
+var configuration = builder.Configuration;
+var httpEndpoint = configuration["HOSPITAL_API_HTTP"];
+var httpsEndpoint = configuration["HOSPITAL_API_HTTPS"];
+
+var hospitalApiUrl = !string.IsNullOrEmpty(httpsEndpoint)
+    ? httpsEndpoint
+    : httpEndpoint;
+
+if (string.IsNullOrEmpty(hospitalApiUrl))
+{
+    throw new InvalidOperationException(
+        "Neither HOSPITAL_API_HTTP nor HOSPITAL_API_HTTPS environment variables are set.");
+}
 
 builder.Services.AddHttpClient("hospital-api", client =>
 {
-    client.BaseAddress = new ("https+http://hospital-api");  
+    client.BaseAddress = new Uri(hospitalApiUrl);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
-}).AddServiceDiscovery();
+});
 
 builder.Services.AddTransient<DataGenerator>();
-
 builder.Services.AddHostedService<RabbitMqProducer>();
 
 var app = builder.Build();
-
 app.Run();
